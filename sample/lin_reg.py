8ff3af81e40a3f2d7553f4960a57f8d20daa9038
tbjag <tbjagodits@gmail.com>
2020-06-04 21:34:49 -0400

    second commit

---
+++ sample/lin_reg.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+
+# Read the data
+X_full = pd.read_csv('../data/housing_data.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = X_full.SalePrice
+X_full.drop(['SalePrice'], axis=1, inplace=True)
+
+# To keep things simple, we'll use only numerical predictors
+X = X_full.select_dtypes(exclude=['object'])
+
+# Break off validation set from training data
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
+
+print(X_train.shape)
+
